be2321de85968fa7fcd19f496ed2a4e57b139e2a
Jason Tokarski <tokarskijason@gmail.com>
2026-07-14 12:44:25 -0500

    Initial project setup

---
+++ src/main/java/com/branch/githubuserservice/GithubuserserviceApplication.java
@@ -0,0 +1,13 @@
+package com.branch.githubuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubuserserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubuserserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/branch/githubuserservice/GithubuserserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.branch.githubuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubuserserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
