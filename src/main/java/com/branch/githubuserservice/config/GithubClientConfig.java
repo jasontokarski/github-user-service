5279c3793c03341b4201b3ce60950159d828c407
Jason Tokarski <tokarskijason@gmail.com>
2026-07-15 15:36:01 -0500

    Updating rest client bean to prevent naming conflicts and adding a virtual thread executor

--- src/main/java/com/branch/githubuserservice/config/GithubClientConfig.java
+++ src/main/java/com/branch/githubuserservice/config/GithubClientConfig.java
@@ -1,5 +1,8 @@
 package com.branch.githubuserservice.config;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,9 +11,14 @@ import org.springframework.web.client.RestClient;
 @Configuration
 public class GithubClientConfig {
     @Bean
-    public RestClient githubRestClient(RestClient.Builder builder, @Value("${github.api.base-url}") String baseUrl) {
-        return builder
+    public RestClient restClient(@Value("${github.api.base-url}") String baseUrl) {
+        return RestClient.builder()
                 .baseUrl(baseUrl)
                 .build();
+    }
+
+    @Bean
+    public ExecutorService virtualThreadExecutor() {
+        return Executors.newVirtualThreadPerTaskExecutor();  // ✅ Java 21 virtual threads
     }
 }
